8fe5e829a422e52a8cf2382609b47822f1ccab65
koldunpodkradun <koldunpodkradun@gmail.com>
2018-06-21 15:29:25 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -3,29 +3,18 @@
         var windowW = windowEl.width();
         var beforeWidth = $(this).width();
 
-        // обновление страницы при масштабировании
-        $(window).resize(function() {
-            var afterWidth = $(this).width();
-            if (afterWidth != beforeWidth) {
-                location.reload()
-            }
-        })
-
         //fix menu
-        if (windowW > 991) {
-            var lastScrollTop = 0;
-            $(window).scroll(function() {
-                var st = $(this).scrollTop();
-                if (st > lastScrollTop) {
-                    $('#header').addClass('header-fix');
-                } else {
-                    $('#header').removeClass('header-fix');
-                }
-            });
-        };
+        var lastScrollTop = 0;
+        $(window).scroll(function() {
+            var st = $(this).scrollTop();
+            if (st > lastScrollTop) {
+                $('#header').addClass('header-fix');
+            } else {
+                $('#header').removeClass('header-fix');
+            }
+        });
 
         //smooth anchor
-
         $(".header-menu, .header-logo, .footer-logo").on("click", "a", function(event) {
             event.preventDefault();
             var id = $(this).attr('href'),
@@ -40,19 +29,17 @@
         /*Динамическая подсветка пунктов меню*/
 
         $(window).scroll(function() {
-            if (windowW > 991) {
-                $('.magic').each(function() {
-                    var window_top = $(window).scrollTop();
-                    var div_top = $(this).offset().top;
-                    var div_1 = $(this).attr('id');
-                    if (window_top > div_top - 120) {
-                        $('.header-menu').find('li').removeClass('menu-active');
-                        $('.header-menu').find('li[class="' + div_1 + '"]').addClass('menu-active');
-                    } else {
-                        $('.header-menu').find('li[class="' + div_1 + '"]').removeClass('menu-active');
-                    };
-                });
-            }
+            $('.magic').each(function() {
+                var window_top = $(window).scrollTop();
+                var div_top = $(this).offset().top;
+                var div_1 = $(this).attr('id');
+                if (window_top > div_top - 120) {
+                    $('.header-menu').find('li').removeClass('menu-active');
+                    $('.header-menu').find('li[class="' + div_1 + '"]').addClass('menu-active');
+                } else {
+                    $('.header-menu').find('li[class="' + div_1 + '"]').removeClass('menu-active');
+                };
+            });
 
         });
 
@@ -119,7 +106,7 @@
 
         /*slider*/
 
-        $('.reviews-slider').slick({
+        $('.reviews-slider').not('.slick-initialized').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
             arrows: false,
@@ -141,7 +128,7 @@
         });
 
 
-        $('.reviews-slider-dots').slick({
+        $('.reviews-slider-dots').not('.slick-initialized').slick({
             slidesToShow: 5,
             slidesToScroll: 1,
             arrows: true,
@@ -191,26 +178,60 @@
             ]
         });
 
+        function sliderResize() {
+            $('.about-items').not('.slick-initialized').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                adaptiveHeight: true
+            });
+
+            $('.services-items').not('.slick-initialized').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                adaptiveHeight: true
+            });
+
+        };
+
         if (windowW < 767) {
-
-            $('.about-items').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                dots: true,
-                adaptiveHeight: true
-            });
-
-            $('.services-items').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                dots: true,
-                adaptiveHeight: true
-            });
-
-
+            sliderResize();
         }
+
+        // resize slider
+        var counter = 0;
+        var i = 0;
+
+        $(window).on('resize orientationchange', function(event) {
+            var windowEl = $('body');
+            var windowW = windowEl.width();
+
+            if (windowW < 767) {
+                counter++
+                if (counter == 1) {
+                    sliderResize();
+                    $('.about-item-wrapp').on('click', function() {
+                        $(this).removeClass('about-item-wrapp-active');
+                    });
+                    i = 1;
+                    counter = 0;
+                }
+            } else if (windowW >= 767 && i == 1) {
+                $('.about-item-wrapp, .works-item').removeAttr('id aria-describedby tabindex role');
+                $('.about-item-wrapp').on('click', function() {
+                    $(this).addClass('about-item-wrapp-active').siblings().removeClass('about-item-wrapp-active');
+                });
+                setTimeout(function() {
+                    $('.about-items, .services-items').slick('unslick');
+                }, 500);
+                counter = 0;
+                i = 0;
+            };
+
+        });
 
 
         /*передает данные о локации, услугах и дате в форму*/
@@ -233,70 +254,9 @@
 
         // fix
         if (windowW > 767) {
-            var about_item_one = $('.about-item-one');
-            var about_item_two = $('.about-item-two');
-            var about_item_three = $('.about-item-three');
-            var about_descr_one_hov = $('.about-descr-one-hov');
-            var about_descr_two_hov = $('.about-descr-two-hov');
-            var about_descr_three_hov = $('.about-descr-three-hov');
-
-            about_item_one.on('click', function() {
-                $(this).addClass('about-item-active');
-                about_item_two.removeClass('about-item-active');
-                about_item_three.removeClass('about-item-active');
-                about_descr_one_hov.addClass('about-descr-hov-active');
-                about_descr_two_hov.removeClass('about-descr-hov-active');
-                about_descr_three_hov.removeClass('about-descr-hov-active');
-            });
-            about_item_two.on('click', function() {
-                $(this).addClass('about-item-active');
-                about_item_one.removeClass('about-item-active');
-                about_item_three.removeClass('about-item-active');
-                about_descr_one_hov.removeClass('about-descr-hov-active');
-                about_descr_two_hov.addClass('about-descr-hov-active');
-                about_descr_three_hov.removeClass('about-descr-hov-active');
-            });
-            about_item_three.on('click', function() {
-                $(this).addClass('about-item-active');
-                about_item_one.removeClass('about-item-active');
-                about_item_two.removeClass('about-item-active');
-                about_descr_one_hov.removeClass('about-descr-hov-active');
-                about_descr_two_hov.removeClass('about-descr-hov-active');
-                about_descr_three_hov.addClass('about-descr-hov-active');
+            $('.about-item-wrapp').on('click', function() {
+                $(this).addClass('about-item-wrapp-active').siblings().removeClass('about-item-wrapp-active');
             });
         }
-
-        /*цвет гамбургера при скролле*/
-        $(window).scroll(function() {
-            var scr = $(this).scrollTop();
-            var about = $('#about').offset();
-            var prices = $('#prices').offset();
-            var services = $('#services').offset();
-            var partners = $('#partners').offset();
-            var reviews = $('#reviews').offset();
-            var contacts = $('#contacts').offset();
-            var hamburger = $('.hamburger');
-
-            if (scr >= about['top']) {
-                hamburger.addClass('hamburger-fix', 300);
-            } else {
-                hamburger.removeClass('hamburger-fix', 300);
-            }
-            if (scr >= prices['top']) {
-                hamburger.removeClass('hamburger-fix', 300);
-            }
-            if (scr >= services['top']) {
-                hamburger.addClass('hamburger-fix', 300);
-            }
-            if (scr >= partners['top']) {
-                hamburger.removeClass('hamburger-fix', 300);
-            }
-            if (scr >= reviews['top']) {
-                hamburger.addClass('hamburger-fix', 300);
-            }
-            if (scr >= contacts['top']) {
-                hamburger.removeClass('hamburger-fix', 300);
-            }
-        });
 
     });
